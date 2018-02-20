727b352a038a5f7ac2aacc6a3ee5e19e5085f479
stevenjmarsh <sjm.atl@gmail.com>
2018-02-19 19:27:18 -0500

    adding fizzbuzz coding exercise

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,1 +1,12 @@
 // code placeholder
+for (i = 1; i < 31; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log(`fizz buzz ${i}`);
+  } else if (i % 3 == 0) {
+    console.log(`fizz ${i}`);
+  } else if (i % 5 == 0) {
+    console.log(`buzz ${i}`);
+  } else {
+    console.log(i);
+  }
+}
