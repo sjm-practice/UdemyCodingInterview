9d8e93c24ab5b708b9e4e2a1f8e7b231f151ee29
stevenjmarsh <sjm.atl@gmail.com>
2018-02-21 17:31:23 -0500

    adding solution for palindrome exercise

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,24 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function reverse(str) {
+  return str.split("").reduce((rStr, char) => char + rStr, "");
+}
+
+function palindrome2(str) {
+  const rStr = reverse(str);
+  return rStr.localeCompare(str) == 0;
+}
+
+function palindrome1(str) {
+  const rStr = str.split("").reverse().join("");
+  return rStr === str;
+}
+
+// less efficient, but neat use of 'every' (compares more chars than needed)
+//    also note the use of indexing in to a string. didn't know / remember that
+function palindrome(str) {
+  return str.split("").every((char, i) => char === str[str.length - 1 - i]);
+}
 
 module.exports = palindrome;
