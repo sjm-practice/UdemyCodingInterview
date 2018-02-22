1ea0bae6f84a7753913bea707e6974fc1793bd68
stevenjmarsh <sjm.atl@gmail.com>
2018-02-21 21:19:41 -0500

    adding solution for maxChar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,40 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  for (let char of str) {
+    charMap[char] = charMap[char]++ || 1;
+  }
+
+  // create an array from the object [['a': 1], ['b': 2], ['c': 1], ...]
+  const tuples = Object.entries(charMap);
+
+  // find the tuple with the highest value
+  const mT = tuples.reduce((maxT, currT) => (currT[1] > maxT[1] ? currT : maxT), ["", 0]);
+
+  return mT[0];
+}
+
+function maxChar1(str) {
+  const strObj = str.split("").reduce((obj, char) => {
+    if (!obj[char]) {
+      obj[char] = 1;
+    } else {
+      obj[char]++;
+    }
+
+    return obj;
+  }, {});
+
+  // create an array from the object [['a': 1], ['b': 2], ['c': 1], ...]
+  const tuples = Object.entries(strObj);
+
+  // find the tuple with the highest value
+  const mT = tuples.reduce((maxT, currT) => (currT[1] > maxT[1] ? currT : maxT), ["", 0]);
+
+  return mT[0];
+}
 
 module.exports = maxChar;
