65ea21653b3800e04649fea7d384d5e368200ef9
stevenjmarsh <sjm.atl@gmail.com>
2018-02-21 19:58:34 -0500

    adding solution for reverseint exercise

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,29 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt2(n) {
+  const isNegative = n < 0;
+  let newNum = n;
+
+  if (isNegative) {
+    newNum = n * -1;
+  }
+
+  let numStr = newNum.toString();
+  numStr = numStr.split("").reverse().join("");
+  newNum = Number.parseInt(numStr, 10);
+
+  if (isNegative) {
+    newNum = newNum * -1;
+  }
+
+  return newNum;
+}
+
+function reverseInt(n) {
+  const newNum = parseInt(n.toString().split("").reverse().join(""), 10);
+
+  return newNum * Math.sign(n); // re-apply original sign
+}
 
 module.exports = reverseInt;
